5653aa4868f269418719809a9f322cb3fb999ed0
mandeep147 <mndpkaur14@gmail.com>
2016-06-26 03:18:54 +0530

    functionality to display result (exit and home button)

--- src/result.java
+++ src/result.java
@@ -1,4 +1,36 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class result {
+import javax.swing.JButton;
 
+public class result extends settings implements ActionListener{
+	private JButton exit,home;
+	void displayResult(){
+		frame("Result");
+		
+		exit=new JButton("Exit");
+		home=new JButton("Home");
+		
+		exit.addActionListener(this);
+		home.addActionListener(this);
+		
+		exit.setBounds(300, 100, 100, 50);
+		home.setBounds(300, 220, 100, 50);
+		
+		Mcq.controlPanel.add(exit);
+		Mcq.controlPanel.add(home);
+		Mcq.mainFrame.add(Mcq.controlPanel);
+		
+		screenDisplay();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==exit){
+			Mcq.mainFrame.dispose();
+		}
+		else if(e.getSource()==home){
+			instructions instr=new instructions();
+			instr.showInstruction();
+		}
+	}
 }
